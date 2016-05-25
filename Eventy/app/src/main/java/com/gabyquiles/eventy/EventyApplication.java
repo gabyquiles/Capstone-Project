1ef32c83a1049ced388dad3216140333de1b40ec
Gabriel Quiles-Perez <info@gabyquiles.com>
2016-05-25 08:37:12 -0400

    Feature: Add free and paid product flavors

--- Eventy/app/src/main/java/com/gabyquiles/eventy/EventyApplication.java
+++ Eventy/app/src/main/java/com/gabyquiles/eventy/EventyApplication.java
@@ -9,6 +9,21 @@ public class EventyApplication extends android.app.Application {
     @Override
     public void onCreate() {
         super.onCreate();
+//        TODO: When event list is empty, the no events is scrollable
+//        TODO: Random crashes
+//        TODO: Notifications for next day events - Is in stash, maybe we have to implement FCM (GCM)???
+//        TODO: use a shareaction provider to invited for event
+//        TODO: use a Content Provider to store templates of events
+//        TODO: Provide widget for next week events with a send invite button
+//        TODO: RTL support
+//        TODO: Handle errors (Net, FB permissions, invalid data in FB)
+//        TODO: Extract all strings
+//        TODO: Add Analytics
+//        TODO: Create free and paid flavors
+//        TODO: Tablet design
+//        TODO: Animated transitions
+//        TODO: Tests
+//        TODO: WebApp
         // Set persistence of DB
 //        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
     }

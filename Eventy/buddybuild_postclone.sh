dcf7372b7748ddeb230b29623f8d9dc56da8ac6e
Gabriel Quiles-Perez <info@gabyquiles.com>
2016-09-29 17:42:06 -0400

    Bug: Fix path where to copy the google-services script

--- Eventy/buddybuild_postclone.sh
+++ Eventy/buddybuild_postclone.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Get the google-services file from BuddyBuild Secure Files and copy it to correct location
-cp ${BUDDYBUILD_SECURE_FILES}/google-services.json ${BUDDYBUILD_WORKSPACE}/google-services.json
+cp ${BUDDYBUILD_SECURE_FILES}/google-services.json ${BUDDYBUILD_WORKSPACE}/app/google-services.json

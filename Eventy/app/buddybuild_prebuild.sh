a58d33836b7cebbb27e1e70b07fa00eedbd7b647
Gabriel Quiles-Perez <info@gabyquiles.com>
2016-09-29 17:32:40 -0400

    Feature: Add BuddyBuild pre build script

---
+++ Eventy/app/buddybuild_prebuild.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Get the google-services file from BuddyBuild Secure Files and copy it to correct location
+cp ${BUDDYBUILD_SECURE_FILES}/google-services.json ${BUDDYBUILD_WORKSPACE}/google-services.json

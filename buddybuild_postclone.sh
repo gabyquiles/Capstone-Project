8b0dff47c6bf50917fb491951306448cf8771e40
Gabriel Quiles-Perez <info@gabyquiles.com>
2016-09-30 10:20:12 -0400

    Bug: Fix path where to copy the google-services.json file

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Running post clone script"
 # Get the google-services file from BuddyBuild Secure Files and copy it to correct location
-cp ${BUDDYBUILD_SECURE_FILES}/google-services.json ${BUDDYBUILD_WORKSPACE}/app/google-services.json
+cp ${BUDDYBUILD_SECURE_FILES}/google-services.json ${BUDDYBUILD_WORKSPACE}/Eventy/app/google-services.json

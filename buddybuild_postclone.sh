5579c0301d6b397bd86e133ce069a4f655c03a70
Gabriel Quiles-Perez <info@gabyquiles.com>
2016-09-30 11:17:31 -0400

    Bug: Create the folder where to put the debug secrets

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -4,4 +4,5 @@ cp ${BUDDYBUILD_SECURE_FILES}/base-google-services.json ${BUDDYBUILD_WORKSPACE}/
 
 cp ${BUDDYBUILD_SECURE_FILES}/free-google-services.json ${BUDDYBUILD_WORKSPACE}/Eventy/app/src/free/google-services.json
 
+mkdir -p ${BUDDYBUILD_WORKSPACE}/Eventy/app/src/debug/res/values/
 cp ${BUDDYBUILD_SECURE_FILES}/debug_google_maps_api.xml ${BUDDYBUILD_WORKSPACE}/Eventy/app/src/debug/res/values/google_maps_api.xml

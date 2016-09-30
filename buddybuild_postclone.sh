cdb2134aa3ce95fa1200da84447425f0584ef7bb
Gabriel Quiles-Perez <info@gabyquiles.com>
2016-09-30 11:05:33 -0400

    Feature: Copy google-services file to the free flavor

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-echo "Running post clone script"
 # Get the google-services file from BuddyBuild Secure Files and copy it to correct location
-cp ${BUDDYBUILD_SECURE_FILES}/google-services.json ${BUDDYBUILD_WORKSPACE}/Eventy/app/google-services.json
+cp ${BUDDYBUILD_SECURE_FILES}/base-google-services.json ${BUDDYBUILD_WORKSPACE}/Eventy/app/google-services.json
+
+cp ${BUDDYBUILD_SECURE_FILES}/free-google-services.json ${BUDDYBUILD_WORKSPACE}/Eventy/app/src/free/google-services.json
